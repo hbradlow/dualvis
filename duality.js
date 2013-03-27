f89042ae5c29a5b133e6bc282106c04c4b052ecf
Kevin Lindkvist <kevinlindkvist@berkeley.edu>
2013-03-26 17:23:36 -0700

    Changed format of dual line

--- duality.js
+++ duality.js
@@ -1,10 +1,18 @@
 function dualize_line(m, b, type)
 {
-    return {'x' : m,  'y' : -b};
+    if (type == "Geometric")
+        return {'x' : m,  'y' : -b};
+    else
+        return {'x' : m,  'y' : -b};
 }
 function dualize_point(x, y, type)
 {
-    return {'slope' : x, 'intersect' : y}; 
+    if (type == "Geometric")
+    {
+        return {'x1' : 0, 'y1' : y, 'x2' : 1, 'y2' : x}
+    }
+    else
+        return {'x1' : 0, 'y1' : y, 'x2' : 1, 'y2' : x}
 }
 
 
